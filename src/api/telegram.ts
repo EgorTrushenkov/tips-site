febd648c0c21cec66ef4944730f87e0f97bde7fb
Egor Trushenkov <etrushenkov@gmail.com>
2025-02-19 13:32:47 +0300

    v2 security token

--- src/api/telegram.ts
+++ src/api/telegram.ts
@@ -1,9 +1,9 @@
-
-const token = process.env.TOKEN
+const token = process.env.API_TOKEN;
+console.log(token);
 const baseUrl = `https://api.telegram.org/bot${token}/`;
 export const sendMessage = async (message: string): Promise<void> => {
   const encodedMessage = encodeURIComponent(message);
   const url = `${baseUrl}sendMessage?chat_id=-1002317037708&text=${encodedMessage}&parse_mode=HTML`;
   const response = await fetch(url);
-  // console.log(response);
+  console.log(response);
 }
